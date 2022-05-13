3e66e0c5cb8cc8af98a88bb7bd2d1acc8f956c75
olaide-hok <olaidekareem43@yahoo.com>
2022-05-13 17:00:13 +0100

    Setup Cart Dropdown

--- src/components/CartDropdown.jsx
+++ src/components/CartDropdown.jsx
@@ -1,15 +1,38 @@
 import React from "react";
 import styled from "styled-components";
+import { useCartContext } from "../context/cart_context";
+import CartItem from "./CartItem";
 
 function CartDropdown() {
+  const { cartItems } = useCartContext();
 
-  
   return (
     <Wrapper>
-      <div className="cart-dropdown">
-        <div className="cart-btn">
-          <button>View Bag</button>
-          <button>Checkout</button>
+      <div className="cart_dropdown">
+        <div>
+          <p className="cart_header">
+            {`My Bag. ${cartItems.length} ${
+              cartItems.length > 1 ? "items" : "item"
+            }`}
+          </p>
+        </div>
+
+        <div className="cart_items">
+          {cartItems.map((item) => {
+            return <CartItem key={item.id} item={item} />;
+          })}
+        </div>
+
+        <div className="total_btn">
+          <div className="total">
+            <p>Total</p>
+            <span>12345</span>
+          </div>
+
+          <div className="cart_btn">
+            <button className="view_bag_btn">View Bag</button>
+            <button className="checkout_btn">Checkout</button>
+          </div>
         </div>
       </div>
     </Wrapper>
@@ -17,23 +40,93 @@ function CartDropdown() {
 }
 
 const Wrapper = styled.div`
-  .cart-dropdown {
+  .cart_header {
+    font-weight: 700;
+    font-size: 16px;
+  }
+
+  .cart_dropdown {
     position: absolute;
-    right: 32px;;
+    right: 32px;
     top: 78px;
     display: flex;
     flex-direction: column;
     width: 325px;
     height: 677px;
-    padding: 16px;
+    padding: 32px 16px;
     border: 2px solid var(--c-primary);
+    background: var(--c-background);
     z-index: 5;
   }
 
-  .cart-btn {
+  .cart_items {
+    display: flex;
+    flex-direction: column;
+    overflow: scroll;
+    overflow-x: hidden;
+
+  }
+
+  .total_btn {
+    margin-top: auto;
+
+  }
+
+  .total {
     display: flex;
     justify-content: space-between;
-    margin-top: auto;
+    align-items: center;
+    margin-bottom: 32px;
+
+    p {
+      font-weight: 500;
+      font-size: 16px;
+      line-height: 18px;
+    }
+
+    span {
+      font-weight: 700;
+      font-size: 16px;
+    }
+  }
+
+  .cart_btn {
+    display: flex;
+    justify-content: space-between;
+    // margin-top: auto;
+
+    .view_bag_btn {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 16px 32px;
+      width: 140px;
+      height: 43px;
+      background: var(--c-background);
+      border: 1px solid #1d1f22;
+      text-align: center;
+      text-transform: uppercase;
+      font-weight: 600;
+      font-size: 14px;
+    }
+
+    .checkout_btn {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 16px 32px;
+      width: 140px;
+      height: 43px;
+      background: var(--c-primary);
+      text-align: center;
+      text-transform: uppercase;
+      font-weight: 600;
+      font-size: 14px;
+      color: var(--c-white);
+      border-style: none;
+    }
   }
 `;
 
---
+++ src/components/CartItem.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+
+function CartItem({ item: { image, pname, price, quantity } }) {
+  return (
+    <Wrapper>
+      <div className="item-details">
+        <p>{pname}</p>
+        <span>{price}</span>
+        <p>Size:</p>
+        <p>Color:</p>
+      </div>
+
+      <div className="quantity">
+        <button> + </button>
+        <span>{quantity}</span>
+        <button> - </button>
+      </div>
+
+      <div className="item_image">
+        <img src={image} alt={pname}></img>
+      </div>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 4fr 1fr 3fr;
+  margin: 32px 0 40px;
+
+  .item_details {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .quantity {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    button {
+      border: 1px solid var(--c-text);
+      background: var(--c-background);
+    }
+
+    span {
+      font-weight: 500;
+      font-size: 16px;
+      text-align: center;
+    }
+  }
+
+  .item_image img {
+    width: 100%;
+  }
+`;
+
+export default CartItem;
